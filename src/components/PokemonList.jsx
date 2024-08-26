706eba02c299616b51d3b1a4a033bcb5b855d599
jungminji <wjdalswl0215@gmail.com>
2024-08-26 16:26:04 +0900

    ✨ update : SweetAlert2 라이브러리 사용

--- src/components/PokemonList.jsx
+++ src/components/PokemonList.jsx
@@ -1,18 +1,12 @@
 import React from "react";
 import styled from "styled-components";
 import PokemonCard from "./PokemonCard";
-import MOCK_DATA from "../mock.js";
 
 const PokemonList = ({ pokemonList }) => {
   // console.log("PokemonList 랜더링");
 
   return (
     <StyledSection>
-      {/* React는 JSX 코드를 JavaScript로 변환할 때, 
-      배열이 반환되면 그 배열의 요소들을 렌더링하여 화면에 출력한다. 
-      즉, [컴포넌트1, 컴포넌트2, 컴포넌트3, ...]와 같은 배열이 반환되면, 
-      React는 이 배열의 각 컴포넌트를 순차적으로 렌더링하고,
-       그것들을 DOM에 추가하기때문에 map 을 사용해보자. */}
       {pokemonList.map((pokemon) => {
         return (
           <PokemonCard key={pokemon.id} pokemon={pokemon} isSelected={false} />
--- src/context/PokemonContext.jsx
+++ src/context/PokemonContext.jsx
@@ -1,4 +1,5 @@
 import { createContext, useContext, useState } from "react";
+import Swal from "sweetalert2";
 
 export const PokemonContext = createContext(null);
 
@@ -14,7 +15,9 @@ export const PokemonProvider = ({ children }) => {
   const addPokemon = (newPokemon) => {
     setSelectedPokemon((prev) => {
       if (prev.length === MY_POKEMON_COUNT) {
-        alert("6 개 넘음. 그만 추가하세요.");
+        Swal.fire(`포켓몬은 ${MY_POKEMON_COUNT}개 까지 추가 가능합니다.
+        포켓몬 한 개를 삭제 후 추가하세요.`);
+
         return prev;
       }
 
@@ -23,7 +26,7 @@ export const PokemonProvider = ({ children }) => {
           return p.id === newPokemon.id;
         })
       ) {
-        alert("똑같은 포켓몬 추가 불가");
+        Swal.fire("똑같은 포켓몬 추가 불가");
         return prev;
       }
 
@@ -38,7 +41,6 @@ export const PokemonProvider = ({ children }) => {
     setSelectedPokemon(updatePokemon);
   };
 
-  // TODO setSelectedPokemon 에 바로 필터해서 넣는 방법임 나중에 수정하기
   //   const removePokemon = (pokemon) => {
   //     setSelectedPokemon((prev) => prev.filter((p) => p.id !== pokemon.id));
   //   };
